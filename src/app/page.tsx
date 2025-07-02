1b7968304a49fbe3b367ab50f4f061ad885d5857
sjay-kennedy <shannonjaykennedy@gmail.com>
2025-07-01 21:42:12 -0500

    commented out custom daisy ui themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     
       <div className="p-4 mt-8">
-          <div className="flex flex-col items-center h-screen mt-[7%] sm:mt-[10%] md:mt-[10%] lg:mt-[15%] xl:mt-[5%] 2xl:mt-[5%]">
+          <div className="flex flex-col items-center  mt-[7%] sm:mt-[10%] md:mt-[10%] lg:mt-[15%] xl:mt-[5%] 2xl:mt-[5%]">
             <Image
               src={theme === "light" ? "/img/nasa-dark.png" : "/img/nasa.png"}
               alt="NASA Logo"
@@ -32,10 +32,20 @@ export default function Home() {
               height={200}
               className="mb-6  animate-fade-in"
             />
-            <h1 className="text-center text-3xl font-bold mb-4">NEO Web Service</h1>
-            <p className="text-center text-lg mb-6 pl-5 pr-5">Explore Near-Earth Objects (NEOs) with NASA's NEO Web Service.</p>
-            <Link href="/browse" className={theme === "light" ? "btn btn-neutral" : "btn btn-secondary"}>Browse Asteroids</Link>
-            
+            <div className={theme === "light" ? "bg-slate-900/40 p-6 rounded-lg shadow-lg" : "bg-pink-300/20 p-6 rounded-lg shadow-lg"}>
+              <h1 className="text-white text-center text-3xl font-bold mb-4">NEO Web Service</h1>
+              <p className="text-white text-center text-lg mb-6 pl-5 pr-5">
+                Explore Near-Earth Objects (NEOs) with NASA's NEO Web Service.
+              </p>
+              <div className="flex justify-center">
+                <Link
+                  href="/browse"
+                  className={theme === "light" ? "btn btn-secondary" : "btn btn-secondary"}
+                >
+                  Browse Asteroids
+                </Link>
+              </div>
+            </div>
           </div>
       </div>
     
